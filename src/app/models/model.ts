a225bced8cd595ae4938f0f390e45b00ed3eeeef
Jeff Lu <ljeff@us.ibm.com>
2025-04-21 19:13:48 -0400

    initial commit

---
+++ src/app/models/model.ts
@@ -0,0 +1,9 @@
+export const EndPoint = {
+  localhost2: 'http://localhost:3000/api/proxy',
+  localhost: 'https://serverless-lkzp79jv0-jeff-lus-projects.vercel.app/api/proxy',
+  production: 'https://serverless-dub0ywlm4-jeff-lus-projects.vercel.app/api/proxy'
+}
+
+const testEndPoints = [
+  'https://jsonplaceholder.typicode.com/posts/1', 'https://api.github.com/repos/vercel/vercel'
+]
